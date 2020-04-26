4e594739c8ae0efe11499405f773e26267f42f4b
April Polubiec <aprilpolubiec@gmail.com>
2020-04-25 21:19:28 -0500

    add stripe - create payment method

--- src/App.js
+++ src/App.js
@@ -12,25 +12,32 @@ import Checkout from './pages/Checkout'
 //Providers
 import { ProductProvider } from './providers/ProductProvider'
 
+//Stripe
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe('pk_test_vHvXhiHMzJ9gItR30MYqY24r00S6o1n3bk')
 function App() {
   return (
     <Router>
-      <ProductProvider>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/products/:itemid'>
-            <ProductDetails />
-          </Route>
-          <Route exact path='/checkout'>
-            <Checkout />
-          </Route>
-          <Route>
-            <Error />
-          </Route>
-        </Switch>
-      </ProductProvider>
+      <Elements stripe={stripePromise}>
+        <ProductProvider>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/products/:itemid'>
+              <ProductDetails />
+            </Route>
+            <Route exact path='/checkout'>
+              <Checkout />
+            </Route>
+            <Route>
+              <Error />
+            </Route>
+          </Switch>
+        </ProductProvider>
+      </Elements>
     </Router>
   )
 }
